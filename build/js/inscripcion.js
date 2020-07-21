65c47e407b4dfc2e032f9114e50ff7a8e8014809
Matias <matiasalarcon55@hotmail.com>
2020-07-21 14:00:41 -0300

    Se separaron los cursos en archivos aparte. Se intentó implementar POO pero había conflictos del uso de las clases Curso y Profesor ya que se podía usar una sola vez. Se volvió al esquema anterior de declarar objetos con su estructura habitual

--- build/js/inscripcion.js
+++ build/js/inscripcion.js
@@ -1,6 +1,10 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
 
@@ -8,39 +12,7 @@ function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o =
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-var agregar = document.getElementById("agregar");
-var eliminar = document.getElementsByClassName("eliminar");
-var inscripcion = document.getElementsByClassName("inscripcion")[0];
-var campos = inscripcion.firstElementChild.cloneNode(true);
-
-for (var i = 0; i < campos.children.length - 1; i++) {
-  campos.children[i].value = "";
-}
-
-var funcionEliminar = function funcionEliminar(elemento) {
-  elemento.addEventListener("click", function () {
-    elemento.parentElement.remove();
-  });
-};
-
-agregar.addEventListener("click", function () {
-  funcionEliminar(campos.lastElementChild);
-  inscripcion.appendChild(campos);
-});
-
-var _iterator = _createForOfIteratorHelper(eliminar),
-    _step;
-
-try {
-  for (_iterator.s(); !(_step = _iterator.n()).done;) {
-    var _boton = _step.value;
-    funcionEliminar(_boton);
-  }
-} catch (err) {
-  _iterator.e(err);
-} finally {
-  _iterator.f();
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Alumno = function Alumno(nombre, apellido, dni) {
   _classCallCheck(this, Alumno);
@@ -48,55 +20,131 @@ var Alumno = function Alumno(nombre, apellido, dni) {
   this.nombre = nombre;
   this.apellido = apellido;
   this.dni = dni;
-};
+}; // Variables
+
 
 var boton = document.getElementById("inscribirse");
-var lista = document.getElementsByClassName("alumno");
-var bandera = 0;
+var bandera = false;
+var agregar = document.getElementById("agregar");
+var eliminar = document.getElementsByClassName("eliminar");
+var inscripcion = document.getElementsByClassName("inscripcion")[0];
+var lista = document.getElementsByClassName("alumno"); // Resetea el formulario cuando se actualiza desde el navegador
+
+document.getElementById("inscripcion").reset(); //Funciones
+
+var funcionEliminar = function funcionEliminar(elemento) {
+  elemento.addEventListener("click", function () {
+    if (lista.length > 3) elemento.parentElement.remove();
+  });
+};
 
 var enlistar = function enlistar() {
-  var inscriptos = []; // let div = document.createElement("div");
-
+  var inscriptos = [];
   var tabla = document.createElement("table");
-  tabla.className = "lista-inscriptos"; // div.appendChild(tabla);
-
+  tabla.className = "lista-inscriptos";
   var cabecera = document.createElement("tr");
-  cabecera.innerHTML = "<th>Nombre</th><th>Apellido</th><th>DNI</th>";
-  tabla.appendChild(cabecera);
+  var titulos = ["Nombre", "Apellido", "DNI"];
 
-  var _iterator2 = _createForOfIteratorHelper(lista),
-      _step2;
-
-  try {
-    for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
-      var _alumno = _step2.value;
-      var nombre = _alumno.children[0].value;
-      var apellido = _alumno.children[1].value;
-      var dni = _alumno.children[2].value;
-      var nuevo = new Alumno(nombre, apellido, dni);
-      inscriptos.push(nuevo);
-    }
-  } catch (err) {
-    _iterator2.e(err);
-  } finally {
-    _iterator2.f();
+  for (var _i = 0, _titulos = titulos; _i < _titulos.length; _i++) {
+    var titulo = _titulos[_i];
+    cabecera.innerHTML += "<th>".concat(titulo, "</th>");
   }
 
-  for (var _i = 0, _inscriptos = inscriptos; _i < _inscriptos.length; _i++) {
-    var alumno = _inscriptos[_i];
+  tabla.appendChild(cabecera);
+
+  var _iterator = _createForOfIteratorHelper(lista),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var _alumno = _step.value;
+      var valores = [];
+
+      var _iterator2 = _createForOfIteratorHelper(_alumno.children),
+          _step2;
+
+      try {
+        for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+          var _dato = _step2.value;
+          if (_dato.value != "") valores.push(_dato.value);
+        }
+      } catch (err) {
+        _iterator2.e(err);
+      } finally {
+        _iterator2.f();
+      }
+
+      valores.pop(); // Elimina el "-" del boton
+
+      if (valores.length == _alumno.children.length - 1) {
+        var nuevo = _construct(Alumno, valores);
+
+        inscriptos.push(nuevo);
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  for (var _i2 = 0, _inscriptos = inscriptos; _i2 < _inscriptos.length; _i2++) {
+    var alumno = _inscriptos[_i2];
     var fila = document.createElement("tr");
-    fila.innerHTML = "<td>".concat(alumno.nombre, "</td><td>").concat(alumno.apellido, "</td><td>").concat(alumno.dni);
+    var datos = "";
+
+    for (var _i3 = 0, _Object$values = Object.values(alumno); _i3 < _Object$values.length; _i3++) {
+      var dato = _Object$values[_i3];
+      datos += "<td>".concat(dato, "</td>");
+    }
+
+    fila.innerHTML = datos;
     tabla.appendChild(fila);
   }
 
-  if (bandera == 0) {
+  if (!bandera) {
     document.getElementById("inscripcion").appendChild(tabla);
-    bandera++;
+    bandera = true;
   } else {
     document.getElementsByClassName("lista-inscriptos")[0].remove();
-    bandera = 0;
+    bandera = false;
     enlistar();
   }
-};
+}; // Eventos
 
+
+var _iterator3 = _createForOfIteratorHelper(eliminar),
+    _step3;
+
+try {
+  for (_iterator3.s(); !(_step3 = _iterator3.n()).done;) {
+    var _boton = _step3.value;
+    funcionEliminar(_boton);
+  }
+} catch (err) {
+  _iterator3.e(err);
+} finally {
+  _iterator3.f();
+}
+
+agregar.addEventListener("click", function () {
+  var campos = inscripcion.firstElementChild.cloneNode(true);
+
+  var _iterator4 = _createForOfIteratorHelper(campos.children),
+      _step4;
+
+  try {
+    for (_iterator4.s(); !(_step4 = _iterator4.n()).done;) {
+      var campo = _step4.value;
+      if (campo.type != "button") campo.value = "";
+    }
+  } catch (err) {
+    _iterator4.e(err);
+  } finally {
+    _iterator4.f();
+  }
+
+  funcionEliminar(campos.lastElementChild);
+  inscripcion.appendChild(campos);
+});
 boton.addEventListener("click", enlistar);
--- src/js/inscripcion.js
+++ src/js/inscripcion.js
@@ -1,26 +1,3 @@
-const agregar = document.getElementById("agregar");
-const eliminar = document.getElementsByClassName("eliminar");
-let inscripcion = document.getElementsByClassName("inscripcion")[0];
-let campos = inscripcion.firstElementChild.cloneNode(true);
-for (let i = 0; i < campos.children.length - 1; i++) {
-  campos.children[i].value = "";
-}
-
-const funcionEliminar = (elemento) => {
-  elemento.addEventListener("click", () => {
-    elemento.parentElement.remove();
-  });
-};
-
-agregar.addEventListener("click", () => {
-  funcionEliminar(campos.lastElementChild);
-  inscripcion.appendChild(campos);
-});
-
-for (let boton of eliminar) {
-  funcionEliminar(boton);
-}
-
 class Alumno {
   constructor(nombre, apellido, dni) {
     this.nombre = nombre;
@@ -29,41 +6,77 @@ class Alumno {
   }
 }
 
+// Variables
 const boton = document.getElementById("inscribirse");
+let bandera = false;
+const agregar = document.getElementById("agregar");
+const eliminar = document.getElementsByClassName("eliminar");
+let inscripcion = document.getElementsByClassName("inscripcion")[0];
 const lista = document.getElementsByClassName("alumno");
-let bandera = 0;
+
+// Resetea el formulario cuando se actualiza desde el navegador
+document.getElementById("inscripcion").reset();
+
+//Funciones
+const funcionEliminar = (elemento) => {
+  elemento.addEventListener("click", () => {
+    if (lista.length > 3) elemento.parentElement.remove();
+  });
+};
 
 const enlistar = () => {
   let inscriptos = [];
-  // let div = document.createElement("div");
   let tabla = document.createElement("table");
   tabla.className = "lista-inscriptos";
-  // div.appendChild(tabla);
   let cabecera = document.createElement("tr");
-  cabecera.innerHTML = "<th>Nombre</th><th>Apellido</th><th>DNI</th>";
+  const titulos = ["Nombre", "Apellido", "DNI"];
+  for (let titulo of titulos) cabecera.innerHTML += `<th>${titulo}</th>`;
   tabla.appendChild(cabecera);
 
   for (let alumno of lista) {
-    const nombre = alumno.children[0].value;
-    const apellido = alumno.children[1].value;
-    const dni = alumno.children[2].value;
-    const nuevo = new Alumno(nombre, apellido, dni);
-    inscriptos.push(nuevo);
+    let valores = [];
+    for (let dato of alumno.children) {
+      if (dato.value != "") valores.push(dato.value);
+    }
+    valores.pop(); // Elimina el "-" del boton
+    if (valores.length == alumno.children.length - 1) {
+      const nuevo = new Alumno(...valores);
+      inscriptos.push(nuevo);
+    }
   }
 
   for (let alumno of inscriptos) {
     let fila = document.createElement("tr");
-    fila.innerHTML = `<td>${alumno.nombre}</td><td>${alumno.apellido}</td><td>${alumno.dni}`;
+    let datos = "";
+    for (let dato of Object.values(alumno)) {
+      datos += `<td>${dato}</td>`;
+    }
+    fila.innerHTML = datos;
     tabla.appendChild(fila);
   }
-  if (bandera == 0) {
+
+  if (!bandera) {
     document.getElementById("inscripcion").appendChild(tabla);
-    bandera++;
+    bandera = true;
   } else {
     document.getElementsByClassName("lista-inscriptos")[0].remove();
-    bandera = 0;
+    bandera = false;
     enlistar();
   }
 };
+
+// Eventos
+for (let boton of eliminar) {
+  funcionEliminar(boton);
+}
+
+agregar.addEventListener("click", () => {
+  let campos = inscripcion.firstElementChild.cloneNode(true);
+  for (let campo of campos.children) {
+    if (campo.type != "button") campo.value = "";
+  }
+  funcionEliminar(campos.lastElementChild);
+  inscripcion.appendChild(campos);
+});
 
 boton.addEventListener("click", enlistar);
